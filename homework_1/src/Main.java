2cbe7985b94b67596ae666d6aa7c6466e2137100
Dqnik <91420415+Dqnik@users.noreply.github.com>
2022-07-29 17:30:24 +0300

    full homework

--- homework_1/src/Main.java
+++ homework_1/src/Main.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
+    static void HW() {
         int x = 0;
-        short y = 0;
-        byte c = 0;
+        int y = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter x");
         if (sc.hasNextInt()) {
@@ -14,22 +12,41 @@ public class Main {
             System.out.println("Error");
         }
         System.out.println("Enter y");
-        if (sc.hasNextShort()) {
-            y = sc.nextShort();
-        }
-        else {
+        if (sc.hasNextInt()) {
+            y = sc.nextInt();
+        } else {
             System.out.println("Error");
         }
-        System.out.println("Enter c");
-        if (sc.hasNextByte()) {
-            c = sc.nextByte();
-        }
-        else {
-            System.out.println("Error");
-        }
-        System.out.println(x + y + c);
-        System.out.println(x - y - c);
-        System.out.println(x * y * c);
-        System.out.println((x / y) / c);
+        System.out.println("x+y-3=" + (x+y-3));
     }
-}
+        public static void main (String[]args){
+            int x = 0;
+            short y = 0;
+            byte c = 0;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter x");
+            if (sc.hasNextInt()) {
+                x = sc.nextInt();
+            } else {
+                System.out.println("Error");
+            }
+            System.out.println("Enter y");
+            if (sc.hasNextShort()) {
+                y = sc.nextShort();
+            } else {
+                System.out.println("Error");
+            }
+            System.out.println("Enter c");
+            if (sc.hasNextByte()) {
+                c = sc.nextByte();
+            } else {
+                System.out.println("Error");
+            }
+            System.out.println(x + y + c);
+            System.out.println(x - y - c);
+            System.out.println(x * y * c);
+            System.out.println((x / y) / c);
+            System.out.println();
+            HW();
+        }
+    }
