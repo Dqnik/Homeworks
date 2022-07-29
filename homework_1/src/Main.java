b6d8462f1309cb13ab2cac0b02d6226a2c35d6a6
Dqnik <91420415+Dqnik@users.noreply.github.com>
2022-07-29 18:39:46 +0300

    opt homework

--- homework_1/src/Main.java
+++ homework_1/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
     static void HW() {
         int x = 0;
@@ -19,6 +18,38 @@ public class Main {
         }
         System.out.println("x+y-3=" + (x+y-3));
     }
+    static int resolveSquare(){
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a");
+        if (sc.hasNextInt()) {
+            a = sc.nextInt();
+        }
+        else {
+            System.out.println("Error");
+        }
+        System.out.println("Enter b");
+        if (sc.hasNextInt()) {
+            b = sc.nextInt();
+        }
+        else {
+            System.out.println("Error");
+        }
+        System.out.println("Enter c");
+        if (sc.hasNextInt()) {
+            c = sc.nextInt();
+        }
+        else {
+            System.out.println("Error");
+        }
+        int i = (b * b) - 4 * (a * c);
+        System.out.println("D = " + i);
+        System.out.println("x1 = " + ((-b + i) / (2*a)));
+        System.out.println("x2 = " + ((-b - i) / (2*a)));
+        return i;
+    }
         public static void main (String[]args){
             int x = 0;
             short y = 0;
@@ -27,26 +58,32 @@ public class Main {
             System.out.println("Enter x");
             if (sc.hasNextInt()) {
                 x = sc.nextInt();
-            } else {
+            }
+            else {
                 System.out.println("Error");
             }
             System.out.println("Enter y");
             if (sc.hasNextShort()) {
                 y = sc.nextShort();
-            } else {
-                System.out.println("Error");
+            }
+            else {
+              System.out.println("Error");
             }
             System.out.println("Enter c");
             if (sc.hasNextByte()) {
                 c = sc.nextByte();
-            } else {
+            }
+            else {
                 System.out.println("Error");
             }
-            System.out.println(x + y + c);
-            System.out.println(x - y - c);
-            System.out.println(x * y * c);
-            System.out.println((x / y) / c);
+            System.out.println("x + y + c = " + (x + y + c));
+            System.out.println("x - y - c = " + (x - y - c));
+            System.out.println("x * y * c = " + (x * y * c));
+            System.out.println("x : y : c = " + (x / y / c));
             System.out.println();
             HW();
+            System.out.println();
+            resolveSquare();
+            sc.close();
         }
     }
