054b8a74d6d6ea2bc99d9b02fd29175efd9efb87
Dqnik <91420415+Dqnik@users.noreply.github.com>
2022-09-03 11:28:18 +0300

    homework_14

--- Hogwarts/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Hogwats_mage hm = new Hogwats_mage();
-        hm.print();
-    }
-}
--- homework_13/src/Maps.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class Maps {
-    public static void main(String[] args) {
-        Map<String, String> map = new HashMap();
-        map.put("Hishnik","a");
-    }
-}
